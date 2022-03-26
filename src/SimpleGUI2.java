bbdd0ce94a0d888cd438ffdcfd1fe80d003863c7
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-26 09:50:05 -0500

    created SimpleGUI2, added event code

---
+++ src/SimpleGUI2.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGUI2 implements ActionListener {
+    JFrame  frame;
+
+    // Main
+    public static void main(String[] args) {
+        SimpleGUI2 gui = new SimpleGUI2();
+        gui.go();
+    } // Main
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Change Colors");
+        button.addActionListener(this);
+
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    } // go
+
+    public void actionPerformed(ActionEvent event) {
+        // When the user clicks, tells the frame to repaint itself. That means the paintComponent() is called on every widget in the frame.
+        frame.repaint();
+    }
+
+} // Class
