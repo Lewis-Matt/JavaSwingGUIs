711f21c3ee2d2fda5f92d56d39d57045769a446b
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-26 10:00:58 -0500

    added comments on use of MyDrawPanel

--- src/SimpleGUI2.java
+++ src/SimpleGUI2.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SimpleGUI2 implements ActionListener {
-    JFrame  frame;
+    JFrame frame;
 
     // Main
     public static void main(String[] args) {
@@ -19,17 +19,19 @@ public class SimpleGUI2 implements ActionListener {
         JButton button = new JButton("Change Colors");
         button.addActionListener(this);
 
+        //
         MyDrawPanel drawPanel = new MyDrawPanel();
 
         frame.getContentPane().add(BorderLayout.SOUTH, button);
+        // drawPanel added to the center.
         frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
-        frame.setSize(300,300);
+        frame.setSize(300, 300);
         frame.setVisible(true);
     } // go
 
     public void actionPerformed(ActionEvent event) {
-        // When the user clicks, tells the frame to repaint itself. That means the paintComponent() is called on every widget in the frame.
+        // When the user clicks, tells the frame to repaint itself. That means the paintComponent() is called on every widget in the frame (using paintComponent method from MyDrawPanel class)
         frame.repaint();
-    }
+    } // actionPerformed
 
 } // Class
